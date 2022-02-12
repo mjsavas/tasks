2c8f1052b9a29f3d2cddd852ad0d0c50d5f54b6b
Michael Savas <mjsavas9@gmail.com>
2022-02-12 15:17:34 -0500

    Basic Html and CSS tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
@@ -7,6 +8,47 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript Michael Savas
             </header>
+            <h1>Elephant</h1>
+            <img
+                src="https://cdn.britannica.com/72/272-004-381F66E5.jpg"
+                alt="This is a picture of a elephant"
+            />
+            <ol>
+                <li>Elephants can live to be 70 years old</li>
+                <li>
+                    Elephants packs are always lead by a head female matriach
+                </li>
+                <li>There are three species of elephants over the world</li>
+            </ol>
+            <Container>
+                <Row>
+                    <Col>
+                        Pros of Elephants: Cute,Friendly, and Smart
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "10px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        Cons of Elephants: None they are the best
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "10px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+
             <p>Hello World</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
